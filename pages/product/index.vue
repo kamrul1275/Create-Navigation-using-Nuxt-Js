b6e98d447971449561e719535bf1c9b5206c0b9f
kamrul1275 <kamrul1275.info@gmail.com>
2024-01-04 14:29:36 +0600

    nav

---
+++ pages/product/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <header>
+      <nav>
+        <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+          <li><NuxtLink to="/profile">Profile Page</NuxtLink></li>
+          <li><NuxtLink to="/product/">Product Page</NuxtLink></li>
+          <li><NuxtLink to="/product/test">Product Test</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
+
+
+
+    <div>Product Home Page</div>
+  </template>
+  
